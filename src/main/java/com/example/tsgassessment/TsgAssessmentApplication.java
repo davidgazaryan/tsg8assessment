a03196275f28e44769dff4c95deb4ed2e962a1cc
York MacBook 081 <yorkmacbook081@Yorks-MacBook-081.local>
2024-06-04 17:19:22 -0400

    initial commit

---
+++ src/main/java/com/example/tsgassessment/TsgAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.tsgassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsgAssessmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsgAssessmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tsgassessment/TsgAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tsgassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsgAssessmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
