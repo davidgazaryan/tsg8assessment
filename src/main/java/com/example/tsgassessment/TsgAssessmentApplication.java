354743b208adb7f1c2630f30d17ecc000a4e69ef
York MacBook 081 <yorkmacbook081@Yorks-MacBook-081.local>
2024-06-12 15:57:05 -0400

    to be cloned

--- src/main/java/com/example/tsgassessment/TsgAssessmentApplication.java
+++ src/main/java/com/example/tsgassessment/TsgAssessmentApplication.java
@@ -2,46 +2,28 @@ package com.example.tsgassessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
-import org.springframework.orm.jpa.JpaTransactionManager;
-import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EnableJpaRepositories("com.example.tsgassessment.repository")
+@EntityScan("com.example.tsgassessment.model")
 public class TsgAssessmentApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TsgAssessmentApplication.class, args);
     }
 
-    @Bean
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
-
-    @Bean
-    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
-        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
-        vendorAdapter.setGenerateDdl(true);
-
-        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
-        factory.setJpaVendorAdapter(vendorAdapter);
-        factory.setPackagesToScan("com.example.tsgassessment.model");
-        factory.setDataSource(dataSource());
-        return factory;
-    }
-
-    @Bean
-    public PlatformTransactionManager transactionManager() {
-        JpaTransactionManager txManager = new JpaTransactionManager();
-        txManager.setEntityManagerFactory(entityManagerFactory().getObject());
-        return txManager;
-    }
-
-
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/tsgassessment/controller/CustomerAPI.java
+++ src/main/java/com/example/tsgassessment/controller/CustomerAPI.java
@@ -1,11 +1,30 @@
 package com.example.tsgassessment.controller;
 
 import com.example.tsgassessment.model.Customer;
+import com.example.tsgassessment.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequestMapping("/customers")
 @RestController
 public class CustomerAPI {
-    Customer customer;
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerAPI(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Customer>> GetAllCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
 }
